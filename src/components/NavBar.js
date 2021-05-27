13621599fb110b0c8936e2e49a55c9eabc464834
henrykorir <longkhery@gmail.com>
2021-05-27 16:53:31 +0300

    Functionalities update and refactor

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,16 @@
 import React from 'react'
  const NavBar = (props) => {
-	 const { onChangeReport } = props
+	 const { onChangeState } = props
 	 console.log(props)
-	 //const handleClick = (value) => {
-	//	onChangeReport(value)
-	 //}
+	 const handleClick = (value) => {
+		onChangeState(value)
+		console.log(value)
+	 }
 	 return(
 		<div className="w-full text-center flex justify-center flex-col bg-green-200 py-2">
 			<ul className="flex space-x-4 text-center items-center text-blue-800">
-				<li><a href="/criteria" onClick={(e) => {e.preventDefault(); onChangeReport(1)}}>criteria</a></li>
-				<li><a href="/patients" onClick={(e) => {e.preventDefault(); onChangeReport(2)}}>patients</a></li>
+				<li><a href="/criteria" onClick={(e) => {e.preventDefault(); handleClick(1)}}>criteria</a></li>
+				<li><a href="/patients" onClick={(e) => {e.preventDefault(); handleClick(2)}}>patients</a></li>
 			</ul>
 		</div>
 	 )
