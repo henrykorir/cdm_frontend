6bfef1ca7785a36dd1826824b534c619d83cf473
henrykorir <longkhery@gmail.com>
2021-05-28 23:00:43 +0300

    Added routes to links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+  BrowserRouter as Router
+  //Switch,
+  //Route,
+ // Link
 } from "react-router-dom";
  const NavBar = (props) => {
 	 const { onChangeState } = props
