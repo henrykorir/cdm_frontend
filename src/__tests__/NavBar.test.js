47b99b782a8cafd832f11ebabc687bd42bc9cb5d
henrykorir <longkhery@gmail.com>
2021-05-29 15:33:49 +0300

    Adding unit tests and refactor

--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import { act, Simulate } from "react-dom/test-utils";
 
 import NavBar from '../components/NavBar'
 
@@ -17,10 +17,23 @@ afterEach(() => {
   container.remove();
   container = null;
 }); 
-
-it('it renders',() =>{
-	act(() =>{
-		render(<NavBar />, container);
+describe('NavBar',() => {
+	it('has anchors with the right texts',()=>{
+		act(() =>{
+			render(<NavBar />, container);
+		});
+		const anchors = container.querySelectorAll('a');
+		expect(anchors[0].textContent).toBe('criteria');
+		expect(anchors[1].textContent).toBe('patients');
 	})
-	expect(container.textContent).toBe("");
+	it('anchors changes states correctly',() => {
+		const onChangeState = jest.fn();
+		act(() =>{
+			render(<NavBar onChangeState={onChangeState}/>, container);
+			container.querySelectorAll('a')[0].dispatchEvent(new MouseEvent("click", { bubbles: true }));
+			container.querySelectorAll('a')[1].dispatchEvent(new MouseEvent("click", { bubbles: true }));
+		})
+		expect(onChangeState).toHaveBeenCalledWith(1);
+		expect(onChangeState).toHaveBeenCalledWith(2);
+	})
 })
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,8 +8,12 @@ import React from 'react'
 	 return(
 		<div style={{"borderBottom": ".5px solid grey"}} className="w-full text-center flex justify-center flex-col bg-green-200 pl-1 py-2">
 			<ul className="flex space-x-4 content-around text-center items-center text-blue-800">
-				<li className="cursor-pointer"><a href="/criteria" onClick={(e) => {e.preventDefault(); handleClick(1)}}>criteria</a></li>
-				<li className="cursor-pointer"><a href="/patients" onClick={(e) => {e.preventDefault(); handleClick(2)}}>patients</a></li>
+				<li className="cursor-pointer">
+					<a href="/criteria" onClick={(e) => {e.preventDefault(); handleClick(1)}}>criteria</a>
+				</li>
+				<li className="cursor-pointer">
+					<a href="/patients" onClick={(e) => {e.preventDefault(); handleClick(2)}}>patients</a>
+				</li>
 			</ul>
 		</div>
 	 )
