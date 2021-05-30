58fbbac23734cd5a04655459539697b3ed31d3bd
henrykorir <longkhery@gmail.com>
2021-05-30 17:57:28 +0300

    Fix: made the table not to render without data

--- src/components/ReportTable.js
+++ src/components/ReportTable.js
@@ -2,44 +2,47 @@ import React from 'react'
 import Link from './Link'
 
 const ReportTable = ({ title, data, isCriteria, onClickLink }) => {
-	const headers = Object.keys(data[0]) 
-	return(
-		<div >
-			<h1 className=" w-full text-center text-white font-bold bg-black">{title}</h1>
-			<table className="table-auto text-left border-collapse w-full border">
-				<thead>
-					<tr>
-						{
-							headers.map((header, index) => <th className="border" key={"" + index}> { header } </th>)
-						}
-					</tr>
-				</thead>
-				<tbody>
-					{ 
-						data.map((row, rowIndex) => 
-							<tr key={"" + rowIndex}>
+	if(data !== undefined && data !== null){
+		const headers = Object.keys(data[0]) 
+		return(
+			<div >
+				<h1 className=" w-full text-center text-white font-bold bg-black">{title}</h1>
+				<table className="table-auto text-left border-collapse w-full border">
+					<thead>
+						<tr>
 							{
-								Object.entries(row).map((entry, entryIndex) =>{
-									return isCriteria ===true && onClickLink !== null  && entry[1] > 0 ?
-										<td className="border" key={"" + entryIndex + rowIndex + 1}>
-											<Link 
-												text={ entry[0] === 'Location' ? entry[1].split(' ').join('-') :  entry[1]} 
-												location={ row.Location }
-												criteria={ entry[0] }
-												onClickLink = { onClickLink } 
-											/>
-										</td> 
-										: 
-										<td className="border" key={"" + entryIndex + rowIndex + 1}> { entry[0] === 'Location' ? entry[1].split(' ').join('-') :  entry[1] } </td>
-								})
+								headers.map((header, index) => <th className="border" key={"" + index}> { header } </th>)
 							}
-							</tr>
-						)
-					}
-					
-				</tbody>
-			</table>
-		</div>
-	)
+						</tr>
+					</thead>
+					<tbody>
+						{ 
+							data.map((row, rowIndex) => 
+								<tr key={"" + rowIndex}>
+								{
+									Object.entries(row).map((entry, entryIndex) =>{
+										return isCriteria ===true && onClickLink !== null  && entry[1] > 0 ?
+											<td className="border" key={"" + entryIndex + rowIndex + 1}>
+												<Link 
+													text={ entry[0] === 'Location' ? entry[1].split(' ').join('-') :  entry[1]} 
+													location={ row.Location }
+													criteria={ entry[0] }
+													onClickLink = { onClickLink } 
+												/>
+											</td> 
+											: 
+											<td className="border" key={"" + entryIndex + rowIndex + 1}> { entry[0] === 'Location' ? entry[1].split(' ').join('-') :  entry[1] } </td>
+									})
+								}
+								</tr>
+							)
+						}
+						
+					</tbody>
+				</table>
+			</div>
+		)
+	}
+	return<div>No data available now</div>
 }
 export default ReportTable
