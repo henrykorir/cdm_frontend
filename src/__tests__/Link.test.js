fa902463c0e1c3a79e596ff65c04bb16e45ad994
henrykorir <longkhery@gmail.com>
2021-05-30 19:57:30 +0300

    Added Link tests

--- src/__tests__/Link.test.js
+++ src/__tests__/Link.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import { act, Simulate } from "react-dom/test-utils";
 
 import Link from '../components/Link'
 
@@ -17,10 +17,42 @@ afterEach(() => {
   container.remove();
   container = null;
 }); 
-
-it('it renders',() =>{
-	act(() =>{
-		render(<Link />, container);
-	})
-	expect(container.textContent).toBe("");
-})
+describe('Link',() => {
+	it('renders an anchor successfully',() =>{
+		act(() =>{
+			render(<Link />, container);
+		})
+		expect(container.textContent).toBe("");
+	});
+	it('renders with a provided text as label', () => {
+		act(()=>{
+			render(<Link text='anchor' />, container)
+		});
+		expect(container.querySelector('a').textContent).toBe('anchor')
+	});
+	it('renders with a provided text as label', () => {
+		const data = {
+				'name':'patient 59',
+				'Encounter Date': '2021-5-31',
+				'Location': 'Location-8',
+				'Known Diabetic': 'New',
+				'Gender': 'M',
+				'Age': 3
+		}
+		const spyOnClick = jest.fn().mockImplementationOnce(() => data);			
+		act(()=>{
+			render(
+				<Link 
+					text='anchor' 
+					location={8}
+					criteria={'New Diabetic'} 
+					onClickLink={spyOnClick}
+				/>,
+				container
+			)
+			//container.querySelectorAll('a')[0].dispatchEvent(new MouseEvent("click", { bubbles: true }));
+			Simulate.click(container.querySelector('a'))
+		});
+		expect(spyOnClick).toHaveBeenCalledTimes(1)
+	});
+});
